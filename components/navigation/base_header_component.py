3b1cab05a2f49efb8d188c9965bdb9e1e39fa7c8
Aleksandr Borisov <BorisaGH@yandex.ru>
2025-10-08 01:15:34 +0300

    refactor replace locator with Link component

--- components/navigation/base_header_component.py
+++ components/navigation/base_header_component.py
@@ -1,13 +1,14 @@
 from playwright.sync_api import Page
 
 from components.base_component import BaseComponent
+from elements.link import Link
 
 
 class BaseHeaderComponent(BaseComponent):
     def __init__(self, page: Page):
         super().__init__(page)
 
-        self.logo_link = page.locator('//*[@id="ecommerce-header"]//a')
+        self.logo_link = Link(page, '//*[@id="ecommerce-header"]//a', "logo")
 
     def click_logo_link(self):
         self.logo_link.click()
