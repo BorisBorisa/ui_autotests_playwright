eca3e3240b9b87fba0e1cee036ab543fc4540cf6
Aleksandr Borisov <BorisaGH@yandex.ru>
2025-10-08 01:18:17 +0300

    refactor replace locators with Button and Text elements

--- components/navigation/auth_header_component.py
+++ components/navigation/auth_header_component.py
@@ -3,11 +3,16 @@ from playwright.sync_api import Page
 from components.navigation.base_header_component import BaseHeaderComponent
 from components.navigation.user_profile_menu_component import UserProfileMenuComponent
 
+from elements.button import Button
+from elements.text import Text
+
 
 class AuthHeaderComponent(BaseHeaderComponent):
     def __init__(self, page: Page):
         super().__init__(page)
 
-        self.card_button = page.locator('//*[@id="ecommerce-header"]//*[@role="button"]')
-        self.card_button_counter = page.locator('//*[@id="ecommerce-header"]//*[@role="button"]//*[text()]')
+        self.card_button = Button(page, '//*[@id="ecommerce-header"]//*[@role="button"]', "card")
+        self.card_button_counter = Text(page, '//*[@id="ecommerce-header"]//*[@role="button"]//*[text()]',
+                                        "card counter")
+
         self.user_profile_menu = UserProfileMenuComponent(page)
