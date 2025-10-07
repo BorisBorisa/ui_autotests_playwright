c67b171592e0362e7cf17f4d4a74d303452e8975
Aleksandr Borisov <BorisaGH@yandex.ru>
2025-10-08 01:10:50 +0300

    refactor replace locators with Button component

--- components/navigation/user_profile_menu_component.py
+++ components/navigation/user_profile_menu_component.py
@@ -2,23 +2,25 @@ from playwright.sync_api import Page, expect
 
 from components.base_component import BaseComponent
 
+from elements.button import Button
+
 
 class UserProfileMenuComponent(BaseComponent):
     def __init__(self, page: Page):
         super().__init__(page)
 
-        self.menu_button = page.locator(".profile .user-name")
-        self.favorites_menu_button = page.locator('//*[text()="Favorites"]')
-        self.log_out_menu_button = page.locator('//*[text()="Log out"]')
+        self.menu_button = Button(page, ".profile .user-name", "menu")
+        self.favorites_menu_button = Button(page, '//*[text()="Favorites"]', "favorites")
+        self.log_out_menu_button = Button(page, '//*[text()="Log out"]', "log out")
 
     def click_user_favorites(self):
         self.menu_button.click()
 
-        expect(self.favorites_menu_button).to_be_visible()
+        self.favorites_menu_button.check_visible()
         self.favorites_menu_button.click()
 
     def click_user_log_out(self):
         self.menu_button.click()
 
-        expect(self.log_out_menu_button).to_be_visible()
+        self.log_out_menu_button.check_visible()
         self.log_out_menu_button.click()
