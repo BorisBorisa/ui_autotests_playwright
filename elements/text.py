94c184a6faab63c25ae52ca93b4ef6e610843967
Aleksandr Borisov <BorisaGH@yandex.ru>
2025-10-08 01:07:53 +0300

    add get text method

--- elements/text.py
+++ elements/text.py
@@ -5,3 +5,7 @@ class Text(BaseElement):
     @property
     def type_of(self) -> str:
         return "text"
+
+    def get_text(self, nth: int = 0, **kwargs) -> str:
+        locator = self.get_locator(nth, **kwargs)
+        return locator.inner_text()
