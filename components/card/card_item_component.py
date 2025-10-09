6f8fb843c8ab93173fdf9893045ac3a791233487
Aleksandr Borisov <BorisaGH@yandex.ru>
2025-10-09 17:15:56 +0300

    refactor card item component (add base card item)

--- components/card/card_item_component.py
+++ components/card/card_item_component.py
@@ -1,48 +1,26 @@
-from components.base_component import BaseComponent
-
 from playwright.sync_api import Page
+
+from components.card.base_card_item_component import BaseCardItemComponent
 
 from elements.text import Text
 from elements.button import Button
-from elements.image import Image
 
 
-class CardItemComponent(BaseComponent):
+class CardItemComponent(BaseCardItemComponent):
     def __init__(self, page: Page):
         super().__init__(page)
 
-        self.image = Image(page, '//*[contains(@class, "cart-list")]//img', "product")
-        self.name = Text(page, '//*[contains(@class, "cart-list")]//h3', "product name")
-        self.remove_button = Button(page, '//button[text()="Remove"]', "product remove")
-
-        self.quantity_decrease_button = Button(page, '//*[text()="Quantity"]/following-sibling::*/*[text()="-"]',
-                                               "decrease")
-        self.quantity_increase_button = Button(page, '//*[text()="Quantity"]/following-sibling::*/*[text()="+"]',
-                                               "increase")
+        self.quantity_decrease_button = Button(
+            page, '//*[text()="Quantity"]/following-sibling::*/*[text()="-"]', "decrease"
+        )
+        self.quantity_increase_button = Button(
+            page, '//*[text()="Quantity"]/following-sibling::*/*[text()="+"]', "increase"
+        )
         self.quantity = Text(page, '//*[text()="Quantity"]/following-sibling::*/span', "quantity")
 
-        self.price = Text(page, '//*[text()="Price"]/following-sibling::*', "price")
-        self.total_price = Text(page, '//*[text()="Total"]/following-sibling::*', "total price")
-
     def check_visible(self, name: str, nth: int = 0, **kwargs):
-        self.image.check_visible(nth, **kwargs)
-
-        self.name.check_visible(nth, **kwargs)
-        self.name.check_have_text(name, nth, **kwargs)
-
-        self.remove_button.check_visible(nth, **kwargs)
+        super().check_visible(name, nth, **kwargs)
 
         self.quantity_decrease_button.check_visible(nth, **kwargs)
         self.quantity_increase_button.check_visible(nth, **kwargs)
         self.quantity.check_visible(nth, **kwargs)
-
-        self.price.check_visible(nth, **kwargs)
-        self.total_price.check_visible(nth, **kwargs)
-
-    def get_price(self, nth: int = 0, **kwargs) -> float:
-        price = self.price.get_text(nth, **kwargs)
-        return float(price.replace("$", ""))
-
-    def get_total_price(self, nth: int = 0, **kwargs) -> float:
-        total_price = self.total_price.get_text(nth, **kwargs)
-        return float(total_price.replace("$", ""))
