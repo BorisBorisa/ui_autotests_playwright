449fc34c36a308979ef1711ac5768b35ec373218
Aleksandr Borisov <BorisaGH@yandex.ru>
2025-10-09 19:33:49 +0300

    add fill from method

--- components/checkout/checkout_information_form_component.py
+++ components/checkout/checkout_information_form_component.py
@@ -13,3 +13,16 @@ class CheckoutInformationFormComponent(BaseComponent):
         self.first_name_input = Input(page, '//*[text()="First Name"]/parent::*/input', "first name")
         self.last_name_input = Input(page, '//*[text()="Last Name"]/parent::*/input', "last name")
         self.zip_code_input = Input(page, '//*[text()="Zip Code"]/parent::*/input', "zip code")
+
+    def fill(self, email: str, first_name: str, last_name: str, zip_code: str):
+        self.email_input.fill(email)
+        self.email_input.check_have_value(email)
+
+        self.first_name_input.fill(first_name)
+        self.first_name_input.check_have_value(first_name)
+
+        self.last_name_input.fill(last_name)
+        self.last_name_input.check_have_value(last_name)
+
+        self.zip_code_input.fill(zip_code)
+        self.zip_code_input.check_have_value(zip_code)
