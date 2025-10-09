b7935d01fb624a0f33337a0dd6831351e68fcaa1
Aleksandr Borisov <BorisaGH@yandex.ru>
2025-10-09 13:59:40 +0300

    add card remove dialog component

---
+++ components/card/remove_dialog_component.py
@@ -0,0 +1,42 @@
+from playwright.sync_api import Page
+
+from components.base_component import BaseComponent
+
+from elements.text import Text
+from elements.button import Button
+
+
+class RemoveDialogComponent(BaseComponent):
+    def __init__(self, page: Page):
+        super().__init__(page)
+
+        self.close_icon_button = Button(page, '//*[@role="dialog"]/button', "close icon")
+
+        self.title = Text(page, '//*[@data-slot="dialog-title"]', "title")
+        self.description = Text(page, '//*[@data-slot="dialog-description"]', "description")
+
+        self.close_button = Button(page, '//button[text()="Close"]', "close")
+        self.remove_button = Button(page, '//button[text()="Remove"]', "remove")
+
+    def check_visible(self):
+        self.close_icon_button.check_visible()
+
+        self.title.check_visible()
+        self.title.check_have_text("Are you absolutely sure?")
+
+        self.description.check_visible()
+        self.description.check_have_text(
+            "This action cannot be undone. This will permanently delete your item from your cart."
+        )
+
+        self.close_button.check_visible()
+        self.remove_button.check_visible()
+
+    def click_close_icon_button(self):
+        self.close_icon_button.click()
+
+    def click_close_button(self):
+        self.close_button.click()
+
+    def click_remove_button(self):
+        self.remove_button.click()
