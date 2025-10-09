5388352e7f7a9d654dbdda03dd1d071c0d11d3ab
Aleksandr Borisov <BorisaGH@yandex.ru>
2025-10-09 18:02:16 +0300

    add checkout information page

---
+++ pages/checkout/checkout_information_page.py
@@ -0,0 +1,22 @@
+from playwright.async_api import Page
+
+from pages.base_page import BasePage
+
+from components.navigation.auth_header_component import AuthHeaderComponent
+from components.checkout.checkout_information_form_component import CheckoutInformationFormComponent
+
+from elements.text import Text
+from elements.button import Button
+
+
+class CheckoutInformationPage(BasePage):
+    def __init__(self, page: Page):
+        super().__init__(page)
+
+        self.header = AuthHeaderComponent(page)
+        self.title = Text(page, '//*[@id="product-sort"]/h3', "title")
+
+        self.checkout_information_form = CheckoutInformationFormComponent(page)
+
+        self.cancel_button = Button(page, '//button/*[text()="Cancel"]', "cancel")
+        self.continue_button = Button(page, '//button/*[text()="Continue"]', "continue")
