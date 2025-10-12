01c13663d9533d3d2871165ff3c6fb527ccad13b
Aleksandr Borisov <BorisaGH@yandex.ru>
2025-10-12 18:09:37 +0300

    add negative tests for invalid credentials

--- tests/authentication/test_authorization.py
+++ tests/authentication/test_authorization.py
@@ -6,11 +6,7 @@ from config import settings
 
 
 class TestAuthorization:
-    def test_successful_authorization(
-            self,
-            login_page: LoginPage,
-            products_page: ProductsPage
-    ):
+    def test_successful_authorization(self, login_page: LoginPage, products_page: ProductsPage):
         login_page.visit(url=AppRoute.LOGIN)
 
         login_page.fill_login_form(
@@ -24,14 +20,68 @@ class TestAuthorization:
         products_page.title.check_visible()
         products_page.products_order_menu.check_visible()
 
-    def test_wrong_email_authorization(self):
-        pass
+    def test_wrong_email_authorization(self, login_page: LoginPage):
+        login_page.visit(url=AppRoute.LOGIN)
 
-    def test_wrong_password_authorization(self):
-        pass
+        login_page.fill_login_form(
+            email=settings.test_data.invalid_email,
+            password=settings.test_user.password
+        )
 
-    def test_wrong_credentials_authorization(self):
-        pass
+        login_page.click_login_button()
+
+        login_page.check_visible_wrong_email_alert()
+        login_page.check_visible_error_email_incorrect_notification()
+
+        login_page.fill_login_form(
+            email="",
+            password=settings.test_user.password
+        )
+
+        login_page.click_login_button()
+
+        login_page.check_visible_empty_email_alert()
+
+    def test_wrong_password_authorization(self, login_page: LoginPage):
+        login_page.visit(url=AppRoute.LOGIN)
+
+        login_page.fill_login_form(
+            email=settings.test_user.email,
+            password=settings.test_data.invalid_password
+        )
+
+        login_page.click_login_button()
+
+        login_page.check_visible_wrong_password_alert()
+        login_page.check_visible_error_password_incorrect_notification()
+
+        login_page.fill_login_form(
+            email=settings.test_user.email,
+            password=""
+        )
+
+        login_page.check_visible_empty_password_alert()
 
     def test_navigate_from_authorization_to_home(self):
         pass
+    def test_wrong_credentials_authorization(self, login_page: LoginPage):
+        login_page.visit(url=AppRoute.LOGIN)
+
+        login_page.fill_login_form(
+            email=settings.test_data.invalid_email,
+            password=settings.test_data.invalid_password
+        )
+
+        login_page.click_login_button()
+
+        login_page.check_visible_wrong_email_alert()
+        login_page.check_visible_wrong_password_alert()
+        login_page.check_visible_error_credentials_incorrect_notification()
+
+        login_page.fill_login_form(
+            email="",
+            password=""
+        )
+
+        login_page.check_visible_empty_email_alert()
+        login_page.check_visible_empty_password_alert()
