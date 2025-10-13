c74690382705d23f5a6b5ab692144434c738e87d
Aleksandr Borisov <BorisaGH@yandex.ru>
2025-10-13 13:39:54 +0300

    add allure steps

--- pages/authentication/login_page.py
+++ pages/authentication/login_page.py
@@ -1,3 +1,4 @@
+import allure
 from playwright.async_api import Page
 
 from pages.base_page import BasePage
@@ -28,6 +29,7 @@ class LoginPage(BasePage):
         self.back_to_home_link = Link(page, '//a[text()="Back to home"]', "back to home")
         self.toast_notification = SonnerToastComponent(page)
 
+    @allure.step("Fill login form")
     def fill_login_form(self, email: str, password: str):
         self.email_input.fill(email)
         self.email_input.check_have_value(email)
@@ -41,18 +43,22 @@ class LoginPage(BasePage):
     def click_back_to_home_link(self):
         self.back_to_home_link.click()
 
+    @allure.step("Check visible wrong email alert")
     def check_visible_wrong_email_alert(self):
         self.email_alert.check_visible()
         self.email_alert.check_have_text("Username is incorrect.")
 
+    @allure.step("Check visible empty email alert")
     def check_visible_empty_email_alert(self):
         self.email_alert.check_visible()
         self.email_alert.check_have_text("Email is a required field")
 
+    @allure.step("Check visible wrong password alert")
     def check_visible_wrong_password_alert(self):
         self.password_alert.check_visible()
         self.password_alert.check_have_text("Password is incorrect.")
 
+    @allure.step("Check visible empty password alert")
     def check_visible_empty_password_alert(self):
         self.password_alert.check_visible()
         self.password_alert.check_have_text("Password is a required field")
