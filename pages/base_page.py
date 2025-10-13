9d5f8b1459c5dfa9a18e0ec08aea17359f6ff84a
Aleksandr Borisov <BorisaGH@yandex.ru>
2025-10-13 13:21:49 +0300

    add allure steps

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,3 +1,5 @@
+import allure
+
 from re import Pattern
 
 from playwright.sync_api import Page, expect
@@ -8,10 +10,22 @@ class BasePage:
         self.page = page
 
     def visit(self, url: str):
-        self.page.goto(url, wait_until='networkidle')
+        step = f"Opening the url '{url}'"
+
+        with allure.step(step):
+            self.page.goto(url, wait_until='networkidle')
 
     def reload(self):
-        self.page.reload(wait_until='networkidle')
+        step = f"Reloading page with url '{self.page.url}'"
+
+        with allure.step(step):
+            self.page.reload(wait_until='networkidle')
 
     def check_current_url(self, expected_url: str | Pattern[str]):
-        expect(self.page).to_have_url(expected_url)
+        if isinstance(expected_url, Pattern):
+            step = f"Checking that current URL matches pattern '{expected_url.pattern}'"
+        else:
+            step = f"Checking that current URL equals '{expected_url}'"
+
+        with allure.step(step):
+            expect(self.page).to_have_url(expected_url)
