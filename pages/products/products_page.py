f0b4253e34852d01e556a3036c7919cfc427f2a5
Aleksandr Borisov <BorisaGH@yandex.ru>
2025-10-14 20:21:51 +0300

    add methods to verify product sorting

--- pages/products/products_page.py
+++ pages/products/products_page.py
@@ -1,3 +1,5 @@
+import allure
+
 from playwright.async_api import Page
 from pages.base_page import BasePage
 
@@ -20,3 +22,23 @@ class ProductsPage(BasePage):
 
     def is_page_opened(self):
         self.title.check_visible()
+
+    @allure.step("Check prices sorted low to high")
+    def check_prices_sorted_low_to_high(self):
+        prices = self.product_card.get_all_prices()
+        assert prices == sorted(prices), f"Product prices are not sorted from low to high: {prices}"
+
+    @allure.step("Check prices sorted high to low")
+    def check_prices_sorted_high_to_low(self):
+        prices = self.product_card.get_all_prices()
+        assert prices == sorted(prices, reverse=True), f"Product prices are not sorted from high to low: {prices}"
+
+    @allure.step("Check prices sorted a to z")
+    def check_names_sorted_a_to_z(self):
+        names = self.product_card.get_all_names()
+        assert names == sorted(names), f"Product names are not sorted alphabetically A → Z: {names}"
+
+    @allure.step("Check prices sorted z to a")
+    def check_names_sorted_z_to_a(self):
+        names = self.product_card.get_all_names()
+        assert names == sorted(names, reverse=True), f"Product names are not sorted alphabetically Z → A: {names}"
