385bfb3979ec18d9307781b30fbfa5e56f77040d
Aleksandr Borisov <BorisaGH@yandex.ru>
2025-10-15 07:49:08 +0300

    add get imagee src methods

--- elements/image.py
+++ elements/image.py
@@ -5,3 +5,7 @@ class Image(BaseElement):
     @property
     def type_of(self) -> str:
         return "image"
+
+    def get_src(self, nth: int = 0, **kwargs) -> str:
+        locator = self.get_locator(nth, **kwargs)
+        return locator.get_attribute("src")
