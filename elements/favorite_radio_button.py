6721543cefb3244d91e5965a47e6ccc75eec071f
Aleksandr Borisov <BorisaGH@yandex.ru>
2025-10-15 07:47:09 +0300

    add check active methods

--- elements/favorite_radio_button.py
+++ elements/favorite_radio_button.py
@@ -6,15 +6,24 @@ from tools.logger import get_logger
 
 logger = get_logger("FAVORITE_RADIO_BUTTON")
 
+
 class FavoriteRadioButton(BaseElement):
     @property
     def type_of(self) -> str:
         return "favorite radio button"
 
-    def is_active(self, nth: int = 0, **kwargs):
+    def _check_active_state(self, active: bool = True, nth: int = 0, **kwargs):
         locator = self.get_locator(nth, **kwargs)
-        step = f'Checking that {self.type_of} "{self.name}" is active'
+        state = "active" if active else "inactive"
+        color = "rgb(255, 0, 0)" if active else "rgb(34, 34, 34)"
+        step = f'Checking that {self.type_of} "{self.name}" is {state}'
 
         with allure.step(step):
             logger.info(step)
-            expect(locator).to_have_attribute("style", "color: rgb(255, 0, 0);")
+            expect(locator).to_have_attribute("style", f"color: {color};")
+
+    def is_active(self, nth: int = 0, **kwargs):
+        self._check_active_state(True, nth, **kwargs)
+
+    def is_inactive(self, nth: int = 0, **kwargs):
+        self._check_active_state(False, nth, **kwargs)
