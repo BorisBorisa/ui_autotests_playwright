14a0b8de09657285708aa4de1d95d5283aada46a
Aleksandr Borisov <BorisaGH@yandex.ru>
2025-10-16 10:07:05 +0300

    refactor improve readability

--- components/base_card_item_component.py
+++ components/base_card_item_component.py
@@ -15,29 +15,27 @@ class BaseCardItemComponent(BaseComponent):
 
         self.image = Image(page, '//*[contains(@class, "cart-list")]//img', "product")
         self.name = Text(page, '//*[contains(@class, "cart-list")]//h3', "product name")
-        self.remove_button = Button(page, '//button[text()="Remove"]', "product remove")
 
         self.price = Text(page, '//*[text()="Price"]/following-sibling::*', "price")
         self.total_price = Text(page, '//*[text()="Total"]/following-sibling::*', "total price")
 
     @allure.step('Check card item visible')
-    def check_visible(self, name: str, nth: int = 0, **kwargs):
-        self.image.check_visible(nth, **kwargs)
+    def check_visible(self, name: str, nth: int = 0):
+        self.image.check_visible(nth)
 
-        self.name.check_visible(nth, **kwargs)
-        self.name.check_have_text(name, nth, **kwargs)
+        self.name.check_visible(nth)
+        self.name.check_have_text(name, nth)
 
-        self.remove_button.check_visible(nth, **kwargs)
+        self.price.check_visible(nth)
+        self.total_price.check_visible(nth)
 
-        self.price.check_visible(nth, **kwargs)
-        self.total_price.check_visible(nth, **kwargs)
 
     @allure.step("Getting product price")
-    def get_price(self, nth: int = 0, **kwargs) -> float:
-        price = self.price.get_inner_text(nth, **kwargs)
+    def get_price(self, nth: int = 0) -> float:
+        price = self.price.get_inner_text(nth)
         return float(price.replace("$", ""))
 
     @allure.step("Getting product total price")
-    def get_total_price(self, nth: int = 0, **kwargs) -> float:
-        total_price = self.total_price.get_inner_text(nth, **kwargs)
+    def get_total_price(self, nth: int = 0) -> float:
+        total_price = self.total_price.get_inner_text(nth)
         return float(total_price.replace("$", ""))
