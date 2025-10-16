cdb281dd8f832acc5d2db78b106ed85369d853a3
Aleksandr Borisov <BorisaGH@yandex.ru>
2025-10-16 04:11:27 +0300

    add product page methods

--- pages/product/product_page.py
+++ pages/product/product_page.py
@@ -1,17 +1,24 @@
 from playwright.async_api import Page
 
-from elements.favorite_radio_button import FavoriteRadioButton
-from elements.input import Input
 from pages.base_page import BasePage
 
+from components.navigation.auth_header_component import AuthHeaderComponent
+from components.sonner_toast_component import SonnerToastComponent
+
+from elements.favorite_radio_button import FavoriteRadioButton
+from elements.input import Input
 from elements.text import Text
 from elements.image import Image
 from elements.button import Button
+
+from tools.data_clases import Product
 
 
 class ProductPage(BasePage):
     def __init__(self, page: Page):
         super().__init__(page)
+
+        self.header = AuthHeaderComponent(page)
 
         self.back_to_products_button = Button(page, '//button[text()="Back to Products"]', "back to products")
 
@@ -21,7 +28,7 @@ class ProductPage(BasePage):
         )
 
         self.name = Text(page, '//*[@class="product-details-wrapper"]//h1', "name")
-        self.description_text = Text(page, '//*[@class="product-details-wrapper"]//p', "description")
+        self.description = Text(page, '//*[@class="product-details-wrapper"]//p', "description")
 
         self.quantity_decrease_button = Button(page, '//button[text()="−"]', "decrease")
         self.quantity_increase_button = Button(page, '//button[text()="+"]', "increase")
