cc06d01b7c0ddec77025b4050d104203ddb480bf
Aleksandr Borisov <BorisaGH@yandex.ru>
2025-10-16 10:09:19 +0300

    add remove button and dialog

--- components/card/card_item_component.py
+++ components/card/card_item_component.py
@@ -3,6 +3,7 @@ import allure
 from playwright.sync_api import Page
 
 from components.base_card_item_component import BaseCardItemComponent
+from components.card.remove_dialog_component import RemoveDialogComponent
 
 from elements.text import Text
 from elements.button import Button
@@ -14,6 +15,8 @@ class CardItemComponent(BaseCardItemComponent):
     def __init__(self, page: Page):
         super().__init__(page)
 
+        self.remove_button = Button(page, '//button[text()="Remove"]', "product remove")
+
         self.quantity_decrease_button = Button(
             page, '//*[text()="Quantity"]/following-sibling::*/*[text()="-"]', "decrease"
         )
@@ -21,6 +24,8 @@ class CardItemComponent(BaseCardItemComponent):
             page, '//*[text()="Quantity"]/following-sibling::*/*[text()="+"]', "increase"
         )
         self.quantity = Text(page, '//*[text()="Quantity"]/following-sibling::*/span', "quantity")
+
+        self.remove_dialog = RemoveDialogComponent(page)
 
     @allure.step('Check card item visible')
     def check_visible(self, name: str, nth: int = 0, **kwargs):
@@ -32,6 +37,9 @@ class CardItemComponent(BaseCardItemComponent):
 
     def _count_products_by_name(self, **kwargs) -> int:
         locator = self.page.locator(self.name.locator.format(**kwargs))
+    def click_remove_button(self, index: int = 0):
+        self.remove_button.click(index)
+
         return locator.count()
 
     def get_all_products(self, **kwargs) -> list[Product]:
