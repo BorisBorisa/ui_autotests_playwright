1328c13e91f108894e7b3f656aa9ad4087299606
Aleksandr Borisov <BorisaGH@yandex.ru>
2025-10-16 04:18:40 +0300

    update remove from favorites test

--- tests/products/test_favorite_products.py
+++ tests/products/test_favorite_products.py
@@ -24,12 +24,12 @@ class TestFavoriteProducts:
         products_page.click_product_favorite_button(0)
         added_products.append(products_page.product_card.get_product(0))
         products_page.check_product_favorite_button_is_active(0)
-        products_page.check_visible_added_to_favorites_notification()
+        products_page.toast_notification.check_visible_added_to_favorites_notification()
 
         products_page.click_product_favorite_button(5)
         added_products.append(products_page.product_card.get_product(5))
         products_page.check_product_favorite_button_is_active(5)
-        products_page.check_visible_added_to_favorites_notification()
+        products_page.toast_notification.check_visible_added_to_favorites_notification()
 
         products_page.reload()
 
@@ -43,24 +43,33 @@ class TestFavoriteProducts:
     def test_removing_product_from_favorites(
             self,
             products_page_with_state: ProductsPage,
+            product_page: ProductPage,
             favorites_page: FavoritesPage
     ):
         products_page = products_page_with_state
         products_page.visit(AppRoute.PRODUCTS)
 
         products_page.click_product_favorite_button(0)
+        products_page.click_product_favorite_button(1)
         products_page.click_product_favorite_button(2)
 
         products_page.click_product_favorite_button(0)
         products_page.check_product_favorite_button_is_inactive(0)
+        products_page.toast_notification.check_visible_remove_from_favorites_notification()
 
-        products_page.check_visible_remove_from_favorites_notification()
+        products_page.click_on_product(1)
+        product_page.is_page_opened()
 
+        product_page.click_product_favorite_button()
+        product_page.toast_notification.check_visible_remove_from_favorites_notification()
+        product_page.click_back_to_product_button()
+
+        products_page.check_product_favorite_button_is_inactive(1)
         products_page.header.user_profile_menu.click_user_favorites()
         favorites_page.is_page_opened()
 
-        favorites_page.click_product_favorite_by_index(0)
-        favorites_page.check_visible_remove_from_favorites_notification()
+        favorites_page.click_product_favorite_button(0)
+        favorites_page.toast_notification.check_visible_remove_from_favorites_notification()
 
         favorites_page.empty_view.check_visible()
         favorites_page.empty_view.click_continue_shopping_link()
