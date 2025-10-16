925c4cae16946c68449f66c749f7c91539a618e2
Aleksandr Borisov <BorisaGH@yandex.ru>
2025-10-16 09:27:32 +0300

    repair test after refactor page objects

--- tests/products/test_favorite_products.py
+++ tests/products/test_favorite_products.py
@@ -23,20 +23,20 @@ class TestFavoriteProducts:
 
         products_page.visit(AppRoute.PRODUCTS)
 
-        products_page.product_card.click_product_favorite_button(0)
+        products_page.product_card.click_favorite_button(0)
         added_products.append(products_page.product_card.get_product(0))
-        products_page.product_card.check_product_favorite_button_is_active(0)
+        products_page.product_card.check_favorite_button_is_active(0)
         products_page.toast_notification.check_visible_added_to_favorites_notification()
 
-        products_page.product_card.click_product_favorite_button(5)
+        products_page.product_card.click_favorite_button(5)
         added_products.append(products_page.product_card.get_product(5))
-        products_page.product_card.check_product_favorite_button_is_active(5)
+        products_page.product_card.check_favorite_button_is_active(5)
         products_page.toast_notification.check_visible_added_to_favorites_notification()
 
         products_page.reload()
 
-        products_page.product_card.check_product_favorite_button_is_active(0)
-        products_page.product_card.check_product_favorite_button_is_active(0)
+        products_page.product_card.check_favorite_button_is_active(0)
+        products_page.product_card.check_favorite_button_is_active(0)
 
         products_page.header.click_user_favorites()
         favorites_page.is_page_opened()
@@ -52,17 +52,17 @@ class TestFavoriteProducts:
     ):
         products_page_with_state.visit(AppRoute.PRODUCTS)
 
-        products_page_with_state.product_card.click_on_product(2)
+        products_page_with_state.product_card.click(2)
         product_page.is_page_opened()
 
-        product_page.click_product_favorite_button()
+        product_page.click_favorite_button()
         product = product_page.get_product()
-        product_page.check_product_favorite_button_is_active()
+        product_page.check_favorite_button_is_active()
         product_page.toast_notification.check_visible_added_to_favorites_notification()
 
         product_page.reload()
 
-        product_page.check_product_favorite_button_is_active()
+        product_page.check_favorite_button_is_active()
 
         product_page.header.click_user_favorites()
         favorites_page.is_page_opened()
@@ -79,26 +79,26 @@ class TestFavoriteProducts:
         products_page = products_page_with_state
         products_page.visit(AppRoute.PRODUCTS)
 
-        products_page.product_card.click_product_favorite_button(0)
-        products_page.product_card.click_product_favorite_button(1)
-        products_page.product_card.click_product_favorite_button(2)
+        products_page.product_card.click_favorite_button(0)
+        products_page.product_card.click_favorite_button(1)
+        products_page.product_card.click_favorite_button(2)
 
-        products_page.product_card.click_product_favorite_button(0)
-        products_page.product_card.check_product_favorite_button_is_inactive(0)
+        products_page.product_card.click_favorite_button(0)
+        products_page.product_card.check_favorite_button_is_inactive(0)
         products_page.toast_notification.check_visible_remove_from_favorites_notification()
 
-        products_page.product_card.click_on_product(1)
+        products_page.product_card.click(1)
         product_page.is_page_opened()
 
-        product_page.click_product_favorite_button()
+        product_page.click_favorite_button()
         product_page.toast_notification.check_visible_remove_from_favorites_notification()
         product_page.click_back_to_product_button()
 
-        products_page.product_card.check_product_favorite_button_is_inactive(1)
+        products_page.product_card.check_favorite_button_is_inactive(1)
         products_page.header.click_user_favorites()
         favorites_page.is_page_opened()
 
-        favorites_page.product_card.click_product_favorite_button(0)
+        favorites_page.product_card.click_favorite_button(0)
         favorites_page.toast_notification.check_visible_remove_from_favorites_notification()
 
         favorites_page.empty_view.check_visible()
