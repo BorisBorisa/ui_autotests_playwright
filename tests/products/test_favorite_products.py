53069737c94081a006479b414cef0c96622d732e
Aleksandr Borisov <BorisaGH@yandex.ru>
2025-10-16 04:19:45 +0300

    add test add product to favorites from product card

--- tests/products/test_favorite_products.py
+++ tests/products/test_favorite_products.py
@@ -40,6 +40,32 @@ class TestFavoriteProducts:
 
         favorites_page.check_favorites_products_equals_expected(added_products)
 
+    def test_add_product_to_favorites_from_product_card(
+            self,
+            products_page_with_state: ProductsPage,
+            product_page: ProductPage,
+            favorites_page: FavoritesPage
+    ):
+        products_page_with_state.visit(AppRoute.PRODUCTS)
+
+        products_page_with_state.click_on_product(2)
+
+        product_page.is_page_opened()
+
+        product_page.click_product_favorite_button()
+        product = product_page.get_product()
+        product_page.check_product_favorite_button_is_active()
+        product_page.toast_notification.check_visible_added_to_favorites_notification()
+
+        product_page.reload()
+
+        product_page.check_product_favorite_button_is_active()
+
+        product_page.header.user_profile_menu.click_user_favorites()
+        favorites_page.is_page_opened()
+
+        favorites_page.check_favorites_products_equals_expected([product])
+
     def test_removing_product_from_favorites(
             self,
             products_page_with_state: ProductsPage,
