64c3d1d4b24a9507282dd29e2b41e8a9fc2262db
Aleksandr Borisov <BorisaGH@yandex.ru>
2025-10-17 23:35:55 +0300

    refactor improve code readability

--- tests/card/test_cart.py
+++ tests/card/test_cart.py
@@ -26,11 +26,13 @@ class TestCart:
 
         products_page.product_card.click_add_to_card_button(0)
         added_products.append(products_page.product_card.get_product(0))
+
         products_page.product_card.check_add_to_cart_button_in_remove_state(0)
         products_page.toast_notification.check_visible_added_to_cart_notification()
 
         products_page.product_card.click_add_to_card_button(2)
         added_products.append(products_page.product_card.get_product(2))
+
         products_page.product_card.check_add_to_cart_button_in_remove_state(2)
         products_page.toast_notification.check_visible_added_to_cart_notification()
 
@@ -44,7 +46,8 @@ class TestCart:
         products_page.header.click_cart_button()
         card_page.is_page_opened()
 
-        card_page.check_cart_products_equals_expected(added_products)
+        card_page.card_item.check_cart_products_equal_expected(added_products)
+
 
     @allure.title("Add products to cart from products card")
     def test_add_product_to_cart_from_product_card(
@@ -67,7 +70,7 @@ class TestCart:
         product_page.header.click_cart_button()
         card_page.is_page_opened()
 
-        card_page.check_cart_products_equals_expected([product])
+        card_page.card_item.check_cart_products_equal_expected([product])
 
     @allure.title("Add products to cart from favorites")
     def test_add_product_to_cart_from_favorites_page(
@@ -93,7 +96,7 @@ class TestCart:
         favorites_page.header.click_cart_button()
         card_page.is_page_opened()
 
-        card_page.check_cart_products_equals_expected([product])
+        card_page.card_item.check_cart_products_equal_expected([product])
 
     def test_removing_product_from_cart(
             self,
@@ -123,18 +126,20 @@ class TestCart:
             card_page: CardPage
     ):
         products_page_with_state.visit(AppRoute.PRODUCTS)
+
         products_page_with_state.product_card.click(3)
         product_page.is_page_opened()
 
         product = product_page.get_product()
+
         product_page.increase_quantity_by(7)
         product_page.decrease_quantity_by(3)
+
         product_page.check_quantity_equals_expected(5)
         product_page.click_add_to_card_button()
 
         product_page.header.click_cart_button()
         card_page.is_page_opened()
 
+        card_page.card_item.check_cart_products_equal_expected([product])
         card_page.card_item.check_quantity_equals_expected(5)
-        card_page.card_item.check_price_equal_expected(product.price, 0)
-        card_page.card_item.check_total_price_equal_expected(product.price, 5, 0)
