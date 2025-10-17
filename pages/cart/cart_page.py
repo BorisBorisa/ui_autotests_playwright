212cf553b1d9dbcce790eec2b51b1a2b75b31bb2
Aleksandr Borisov <BorisaGH@yandex.ru>
2025-10-17 23:35:29 +0300

    refactor remove check_cart_products_equals_expected method

--- pages/cart/cart_page.py
+++ pages/cart/cart_page.py
@@ -1,4 +1,3 @@
-import allure
 from playwright.async_api import Page
 
 from pages.base_page import BasePage
@@ -10,8 +9,6 @@ from components.card.card_empty_view_component import CardEmptyViewComponent
 
 from elements.text import Text
 from elements.button import Button
-
-from tools.data_clases import Product
 
 class CardPage(BasePage):
     def __init__(self, page: Page):
@@ -33,11 +30,5 @@ class CardPage(BasePage):
         self.title.check_visible()
         self.title.check_have_text("Your Cart")
 
-    def check_cart_products_equals_expected(self, expected_products: list[Product]):
-        actual_products = self.card_item.get_all_products()
-
-        expected_sorted = sorted(expected_products)
-        actual_sorted = sorted(actual_products)
-
-        with allure.step("Verify that cart products match the expected ones"):
-            assert expected_sorted == actual_sorted, f"Expected: {expected_sorted}\nActual: {actual_sorted}"
+    def click_checkout_button(self):
+        self.checkout_button.click()
