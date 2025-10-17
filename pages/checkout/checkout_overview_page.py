4b2959458112381414e259edc6a8f72b9db4335f
Aleksandr Borisov <BorisaGH@yandex.ru>
2025-10-17 23:40:24 +0300

    refactor delete get_items_price and get_total_price methods

--- pages/checkout/checkout_overview_page.py
+++ pages/checkout/checkout_overview_page.py
@@ -44,9 +44,6 @@ class CheckoutOverviewPage(BasePage):
     def click_cancel_button(self):
         self.cancel_button.click()
 
-    def get_total_price(self) -> float:
-        text = self.total_price.get_inner_text()
-        return float(text.split("$")[1])
     def check_visible_checkout_summary(self):
         self.payment_info_text.check_visible()
         self.shipping_info_text.check_visible()
