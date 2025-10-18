aa267ff58ec818d94be44717d60f388b9a6704d6
Aleksandr Borisov <BorisaGH@yandex.ru>
2025-10-18 09:29:30 +0300

    add attach videos to allure

--- tools/playwright/pages.py
+++ tools/playwright/pages.py
@@ -1,5 +1,7 @@
-from playwright.sync_api import Playwright
+import allure
 
+from playwright.sync_api import Playwright, Page
+from typing import Generator
 from config import settings, Browser
 
 
@@ -8,15 +10,20 @@ def initialize_playwright_page(
         test_name: str,
         browser_type: Browser,
         storage_state: str | None = None
-):
+) -> Generator[Page, None, None]:
     browser = playwright[browser_type].launch(headless=settings.headless)
     context = browser.new_context(
         base_url=settings.get_base_url,
+        record_video_dir=settings.videos_dir,
         storage_state=storage_state
     )
     context.tracing.start(screenshots=True, snapshots=True, sources=True)
+    page = context.new_page()
 
-    yield context.new_page()
+    yield page
 
     context.tracing.stop(path=settings.tracing_dir.joinpath(f'{test_name}.zip'))
     browser.close()
+
+    allure.attach.file(settings.tracing_dir.joinpath(f'{test_name}.zip'), name='trace', extension='zip')
+    allure.attach.file(page.video.path(), name='video', attachment_type=allure.attachment_type.WEBM)
