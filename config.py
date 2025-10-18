b35e49c941c6c1182ac87ac7882d807f8f3f0e8f
Aleksandr Borisov <BorisaGH@yandex.ru>
2025-10-18 09:07:22 +0300

    add tracing dir path

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 from enum import Enum
 from typing import Self
 
-from pydantic import BaseModel, FilePath, EmailStr, HttpUrl
+from pydantic import BaseModel, FilePath, EmailStr, HttpUrl, DirectoryPath
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
@@ -37,6 +37,7 @@ class Settings(BaseSettings):
     browsers: list[Browser]
     test_user: TestUser
     test_data: TestData
+    tracing_dir: DirectoryPath
     browser_state_file: FilePath
 
     @property
@@ -45,12 +46,15 @@ class Settings(BaseSettings):
 
     @classmethod
     def initialize(cls) -> Self:
+        tracing_dir = DirectoryPath("./tracing")
         browser_state_file = FilePath("./.auth/browser-state.json")
 
+        tracing_dir.mkdir(exist_ok=True)
         browser_state_file.parent.mkdir(parents=True, exist_ok=True)
         browser_state_file.touch(exist_ok=True)
 
         return Settings(
+            tracing_dir=tracing_dir,
             browser_state_file=browser_state_file
         )
 
